50fe5c85d04ba37679c0cde2d380a8f2b70f55de
程雷鸣 <261538015@qq.com>
2012-09-25 16:57:17 +0800

    Init System
    core

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    define('THINK_PATH','./App/ThinkPHP/');
+    //define('RUNTIME_ALLINONE',1);
+    define('APP_NAME','App');
+    define('APP_PATH','./App/');
+    define('APP_DEBUG',1);
+    define('APP_PUBLIC_PATH','../Public');
+    require(THINK_PATH.'/ThinkPHP.php');
+?>
